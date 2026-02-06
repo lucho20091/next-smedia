c2dbbba4aa223ad2f345d9b019675d2d68022989
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2026-02-06 11:49:05 -0500

    Reduce maximum media size limit to 10MB

--- components/CreatePost.jsx
+++ components/CreatePost.jsx
@@ -27,8 +27,8 @@ export default function CreatePostPage({ isAdmin = false }) {
       return;
     }
 
-    if (file.size > 1024 * 1024 * 40) {
-      showToast("Media size must be less than 40MB", "error");
+    if (file.size > 1024 * 1024 * 10) {
+      showToast("Media size must be less than 10MB", "error");
       return;
     }
 
